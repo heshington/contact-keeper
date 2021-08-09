d5d97af6a0d6728192cd0c1aa1103c2bb52f878a
Hiren <Hiren@pikpok.com>
2021-08-09 17:29:56 +1200

    added more

--- client/src/componants/pages/Home.js
+++ client/src/componants/pages/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
-
- const Home = () => {
-    return (
-        <div>
-            <h1>Home
-            </h1>
-        </div>
-    )
-}
+import React from 'react';
+import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
+const Home = () => {
+  return (
+    <div className='grid-2'>
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <Contacts />
+      </div>
+    </div>
+  );
+};
 export default Home;
