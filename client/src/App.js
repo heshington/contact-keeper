013a5ed794c6518d090723190b4351c09992d70a
Hiren <Hiren@pikpok.com>
2021-08-12 17:22:13 +1200

    Added more work

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,31 @@ import Home from './componants/pages/Home';
 import About from './componants/pages/About';
 import Regsiter from './componants/auth/Register';
 import Login from './componants/auth/Login';
+import Alerts from './componants/layout/Alerts';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home}></Route>
-                <Route exact path='/about' component={About}></Route>
-                <Route exact path='/register' component={Regsiter}></Route>
-                <Route exact path='/login' component={Login}></Route>
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home}></Route>
+                  <Route exact path='/about' component={About}></Route>
+                  <Route exact path='/register' component={Regsiter}></Route>
+                  <Route exact path='/login' component={Login}></Route>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
